3376b6750854b6d63d303244ba51c61522b08c8c
harmonious-cache-6099 <isaac_scothern@hotmail.co.uk>
2020-03-10 18:47:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object;
 }
 
-function  destructivelyDeleteFromObjectByKey(object, key) {
+function  DeleteFromObjectByKey(object, key) {
   delete object.key;
   return object;
 }
